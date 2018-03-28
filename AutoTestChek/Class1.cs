96cded80839447c25f7b530b17e92780853735ba
MikeKurshakov <kurshakov.mihail@gmail.com>
2018-03-28 12:50:11 +0300

    Add project files.

---
+++ AutoTestChek/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoTestChek
+{
+    public class Class1
+    {
+    }
+}
